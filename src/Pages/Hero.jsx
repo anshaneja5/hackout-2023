22bc458350aa7e1174851175672aad1bf91e89d0
Abhishek Sharma <abhishek79009@gmail.com>
2023-11-04 22:09:01 +0530

    fav icon and title added

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
   }, []);
 
   const particlesLoaded = useCallback(async (container) => {
-    await console.log(container);
+    await console.log();
   }, []);
 
   return (
